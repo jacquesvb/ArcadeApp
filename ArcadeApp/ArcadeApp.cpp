4989187c136e8a21e2f126b7874d4ab71729a65d
Jacques Van Blokland <jacquesvb@live.com>
2020-09-07 01:17:46 -0700

    First

---
+++ ArcadeApp/ArcadeApp.cpp
@@ -0,0 +1,86 @@
+// ArcadeApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include "Color.h"
+
+const int SCREEN_WIDTH = 224;
+const int SCREEN_HEIGHT = 288;
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y);
+size_t GetIndex(SDL_Surface* noptrSurface, int r, int c);
+
+int main(int argc, const char * argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		std::cout << "Error SDL_Init Failed" << std::endl;
+		return 1;
+	}
+
+	SDL_Window* optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+	if (optrWindow == nullptr)
+	{
+		std::cout << "Could not create window, got error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Surface* noptrWindowSurface = SDL_GetWindowSurface(optrWindow);
+	noptrWindowSurface = SDL_ConvertSurfaceFormat(noptrWindowSurface, SDL_PIXELFORMAT_ARGB8888, 0);
+
+	SDL_PixelFormat* pixelFormat = noptrWindowSurface->format;
+
+	// uint32_t color = 0xFFFF0000;
+	Color::InitColorFormat(pixelFormat);
+
+	Color c(255, 255, 0, 255);
+	std::cout << "The window pixel format is: " << SDL_GetPixelFormatName(pixelFormat->format);
+
+	SetPixel(noptrWindowSurface, c.GetPixelColor(), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	// SetPixel(noptrWindowSurface, color, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+
+	SDL_UpdateWindowSurface(optrWindow);
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+	while (running)
+	{
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(optrWindow);
+	SDL_Quit();
+
+	return 0;
+}
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y)
+{
+	SDL_LockSurface(noptrWindowSurface);
+
+	uint32_t* pixels = (uint32_t*)noptrWindowSurface->pixels;
+
+	size_t index = GetIndex(noptrWindowSurface, y, x);
+
+	pixels[index] = color;
+
+	SDL_UnlockSurface(noptrWindowSurface);
+}
+
+size_t GetIndex(SDL_Surface* noptrSurface, int r, int c)
+{
+	return r * noptrSurface->w + c;
+}
+
---
+++ ArcadeApp/Vec2D.cpp
@@ -0,0 +1,152 @@
+#include "Vec2D.h"
+#include "Utils.h"
+#include <cassert>
+#include <cmath>
+
+const Vec2D Vec2D::Zero;
+
+std::ostream& operator << (std::ostream& consoleOut, const Vec2D& vec)
+{
+	std::cout << "X: " << vec.mX << ", Y: " << vec.mY << std::endl;
+	return consoleOut;
+}
+
+Vec2D operator*(float scalar, const Vec2D& vec)
+{
+	return vec * scalar;
+}
+
+
+bool Vec2D::operator==(const Vec2D& vec2) const
+{
+	return IsEqual(mX, vec2.mX) && IsEqual(mY, vec2.mY);
+}
+
+bool Vec2D::operator!=(const Vec2D& vec2) const
+{
+	return !(*this == vec2);
+}
+
+/* Negating a vector
+v = [0, 3]
+-v = [0, -3]
+/|\		|
+ |		|
+ |	   \|/
+ v = [2, 0]
+ -->
+ -v = [-2, 0]
+ <--
+ */
+
+Vec2D Vec2D::operator-() const
+{
+	return Vec2D(-mX, -mY);
+}
+
+/* Vector multiply by a scalar
+	v = [1, 0]
+	->
+	3 * v = [3, 0]
+	--->
+*/
+
+Vec2D Vec2D::operator*(float scale) const
+{
+	return Vec2D(scale * mX, scale * mY);
+}
+
+Vec2D Vec2D::operator/(float scale) const
+{
+	assert(fabsf(scale) > EPSILON);
+	return Vec2D(mX / scale, mY / scale);
+}
+
+Vec2D& Vec2D::operator*=(float scale)
+{
+	*this = *this * scale;
+	return *this;
+}
+
+Vec2D& Vec2D::operator/=(float scale)
+{
+	assert(fabsf(scale) > EPSILON);
+	*this = *this / scale;
+	return *this;
+}
+
+/* Vector addition and subtraction
+	v = [1, 4]
+	a = [2, -2]
+	v + a = [1, 4] + [2, -2] = [3, 2]
+	v - a = [1, 4] - [2, -2] = [-1, 6]
+*/
+
+Vec2D Vec2D::operator+(const Vec2D& vec) const
+{
+	return Vec2D(mX + vec.mX, mY + vec.mY);
+}
+
+Vec2D Vec2D::operator-(const Vec2D& vec) const
+{
+	return Vec2D(mX - vec.mX, mY - vec.mY);
+}
+
+Vec2D& Vec2D::operator+=(const Vec2D vec)
+{
+	*this = *this + vec;
+	return *this;
+}
+
+Vec2D& Vec2D::operator-=(const Vec2D vec)
+{
+	*this = *this - vec;
+	return *this;
+}
+
+/*
+	  /|
+hyp  / | y
+	/  |
+	----
+	  x
+*/
+
+float Vec2D::Mag2() const
+{
+	return mX * mX + mY * mY;
+}
+
+float Vec2D::Mag() const
+{
+	return sqrt(Mag2());
+}
+
+Vec2D Vec2D::GetUnitVec() const
+{
+	float mag = Mag();
+
+	if (mag > EPSILON)
+	{
+		return *this / mag;
+	}
+
+	return Vec2D::Zero;
+}
+
+Vec2D Vec2D::Normalize()
+{
+	float mag = Mag();
+
+	if (mag > EPSILON)
+	{
+		*this /= mag;
+	}
+
+	return *this;
+}
+
+float Vec2D::Distance(const Vec2D& vec) const
+{
+	return (vec - *this).Mag();
+}
